6a1c5aa6b2792fd020081249c44481146daeb465
byronogis <byronogis@outlook.com>
2025-01-22 14:00:59 +0800

    chore(unmini): rename polyfill export while import from root export

--- packages/unmini/src/index.ts
+++ packages/unmini/src/index.ts
@@ -3,7 +3,9 @@ import type { Config } from '@unmini/config'
 export * from '@unmini/cli'
 export * from '@unmini/config'
 export * from '@unmini/core'
-export * from '@unmini/polyfill'
+export {
+  unmini as polyfill,
+} from '@unmini/polyfill'
 
 export function defineConfig(config: Config): Config {
   return config
