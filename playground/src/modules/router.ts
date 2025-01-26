321e728ae588e59aa3fe43eada2fc2aef19d1484
byronogis <byronogis@outlook.com>
2025-01-26 17:22:41 +0800

    chore(playground): add catch-all route to redirect to home

--- playground/src/modules/router.ts
+++ playground/src/modules/router.ts
@@ -4,6 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 export const install: UserModule = ({ app }) => {
+  routes.push({
+    path: '/:pathMatch(.*)*',
+    redirect: '/unmini/home',
+  })
+
   const router = createRouter({
     history: createWebHistory(),
     routes: setupLayouts(routes),
